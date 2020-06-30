8b63668019367e1ec63d09af28507842259639a3
lujain <63684917+lujainalkandari@users.noreply.github.com>
2020-06-30 22:58:36 +0300

    🦊

---
+++ ios-cw5-p1-p2-p3/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ios-cw5-p3
+//
+//  Created by Lujain Alkandari on 6/28/20.
+//  Copyright © 2020 Lujain Alkandari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var turnLabel: UILabel!
+    
+    
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var b4: UIButton!
+    @IBOutlet weak var b5: UIButton!
+    @IBOutlet weak var b6: UIButton!
+    @IBOutlet weak var b7: UIButton!
+    @IBOutlet weak var b8: UIButton!
+    @IBOutlet weak var b9: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
